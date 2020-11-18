5b62875d88e2fc9c4ab166b0a7a9da9fbb790bb3
Bob Freid <freidrd@mail.uc.edu>
2020-11-18 17:43:28 -0500

    Complete the fileds for the Venue class with data annotations

--- Models/DomainModels/Venue.cs
+++ Models/DomainModels/Venue.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,18 @@ namespace MvcTravel.Models
 {
 	public class Venue
 	{
+		public int VenueId { get; set; }
+
+		[StringLength(100)]
+		[Required]
+		public string VenueName { get; set; }
+
+		[StringLength(100)]
+		[Required]
+		public string VenueAddress { get; set; }
+
+		[StringLength(100)]
+		[Required]
+		public String VenueContactInfo { get; set; }
 	}
 }
