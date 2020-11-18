d3b16d2b1f4bb3ae2381e6beda7821e478176590
Bob Freid <freidrd@mail.uc.edu>
2020-11-18 17:30:30 -0500

    Created folder for entities and defined the Event class

---
+++ Models/DomainModels/Event.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcTravel.Models
+{
+	public class Event
+	{
+		public int EventId { get; set; }
+
+		[StringLength(100)]
+		[Required]
+		public string EventName { get; set; }
+
+		[StringLength (50)]
+		[Required]
+		public string EventCity { get; set; }
+
+		[DataType(DataType.DateTime)]
+		[DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+		[Required]
+		public DateTime EventDate { get; set; }
+	}
+}
