2a7f260bc090b93a62e3cd82170b4294f174ef03
Bob Freid <freidrd@mail.uc.edu>
2020-11-18 21:48:49 -0500

    Created my MvcTravelContext file to bring in the Entity Framework to manage my database

---
+++ Models/DataLayer/MvcTravelContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MvcTravel.Models.DomainModels;
+
+namespace MvcTravel.Models.DataLayer
+{
+	public class MvcTravelContext : DbContext
+	{
+		public MvcTravelContext(DbContextOptions<MvcTravelContext> options) : base(options) { }
+		public DbSet<Event> Events { get; set; }
+		
+		public DbSet<Venue> Venues { get; set; }
+		public DbSet<Promoter> Promoters { get; set; }
+	}
+	
+}
