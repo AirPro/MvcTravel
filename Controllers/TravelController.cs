ebae5f7e146610522c9d049d92b9c2124de9036c
Bob Freid <freidrd@mail.uc.edu>
2020-11-21 17:40:01 -0500

    Basic no changes

--- Controllers/TravelController.cs
+++ Controllers/TravelController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Models.DomainModels;
+using MvcTravel.Models.DataLayer;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +10,40 @@ namespace MvcTravel.Controllers
 {
 	public class TravelController : Controller
 	{
+		private ClassMvcTravelUnitOfWork data { get; set; }
+		public TravelController(MvcTravelContext ctx) => data = new ClassMvcTravelUnitOfWork(ctx);
+
+		public ViewResult Index(int id)
+		{
+            // options for Event query
+            var eventOptions = new QueryOptions<Event>
+            {
+                OrderBy = e => e.EventId
+            };
+
+            // options for Venue query
+            var venueOptions = new QueryOptions<Venue>
+            {
+                Includes = "Venue Name, Promoter Name"
+            };
+            // order by day and then time if no filter. Otherwise, filter by day and order by time.
+            if (id == 0)
+            {
+                venueOptions.OrderBy = e => e.VenueId;
+                //venueOptions.ThenOrderBy = e => c.MilitaryTime;
+            }
+            else
+            {
+                //venueOptions.Where = e => Id == id;
+                venueOptions.OrderBy = e => e.VenueName;
+            }
+
+            // execute queries
+            ViewBag.Events = data.Events.List(eventOptions);
+            return View(data.Events.List(eventOptions));
+
+        }
+
 		public IActionResult Index()
 		{
 			return View();
