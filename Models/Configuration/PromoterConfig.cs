8190625e955dbe8940860a559627440794f8c83e
Bob Freid <freidrd@mail.uc.edu>
2020-11-23 12:50:49 -0500

    Develop configuration files for seeding database - not necessary in normal environment.

--- Models/Configuration/PromoterConfig.cs
+++ Models/Configuration/PromoterConfig.cs
@@ -13,7 +13,7 @@ namespace MvcTravel.Models.Configuration
 		public void Configure(EntityTypeBuilder<Promoter> entity)
 		{
 			entity.HasData(
-				new Promoter { PromoterId = 1, PromoterName = "Promoter Name 1", PromoterAddress = "Promoter Address 1", PromoterContactInfo = "Promoter Contact Info 1"}
+				new Promoter { PromoterId = 1, PromoterName = "Promoter Name 1", PromoterAddress = "Promoter Address 1", PromoterContactInfo = "Promoter Contact Info 1"},
 				new Promoter { PromoterId = 2, PromoterName = "Promoter Name 2", PromoterAddress = "Promoter Address 2", PromoterContactInfo = "Promoter Contact Info 2" }
 
 			);
--- Models/DataLayer/MvcTravelContext.cs
+++ Models/DataLayer/MvcTravelContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
+using MvcTravel.Models.Configuration;
 using MvcTravel.Models.DomainModels;
 
 namespace MvcTravel.Models.DataLayer
