ecd8abed5603eed9c005ade7cc1189f7078316cb
Bob Freid <freidrd@mail.uc.edu>
2020-11-23 18:19:08 -0500

    Index for Venue and VenueController adjsutment

--- Controllers/VenueController.cs
+++ Controllers/VenueController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MvcTravel.Models.DataLayer;
+using MvcTravel.Models.DomainModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,30 @@ namespace MvcTravel.Controllers
 {
 	public class VenueController : Controller
 	{
-		public IActionResult Index()
-		{
-			return View();
-		}
-	}
+		private ClassMvcTravelUnitOfWork data { get; set; }
+		public VenueController(MvcTravelContext ctx) => data = new ClassMvcTravelUnitOfWork(ctx);
+
+		public ViewResult Index(int id)
+        {
+            // options for Venue query
+            var venueOptions = new QueryOptions<Venue>
+            {
+                OrderBy = v => v.VenueId
+            };
+
+            if (id == 0)
+            {
+               venueOptions.OrderBy = v => v.VenueId;
+            }
+            else
+            {
+                venueOptions.OrderBy = v => v.VenueName;
+            }
+
+            // execute queries
+            ViewBag.Venues = data.Venues.List(venueOptions);
+            return View(data.Venues.List(venueOptions));
+
+        }
+    }
 }
