6d7881c5199b13aabb2b8d15d2bf3ee4750e40b1
Bob Freid <freidrd@mail.uc.edu>
2020-11-23 14:59:15 -0500

    Created controllers and Views for Promoter and the Venue classes

---
+++ Controllers/PromoterController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcTravel.Controllers
+{
+	public class PromoterController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
